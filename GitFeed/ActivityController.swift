a0364aaf9b88de2c54869b1db0686d02b3922ec7
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2022-09-03 14:56:53 +0900

    Transforming the response

--- GitFeed/ActivityController.swift
+++ GitFeed/ActivityController.swift
@@ -64,7 +64,24 @@ class ActivityController: UITableViewController {
   }
 
   func fetchEvents(repo: String) {
-
+    let response = Observable.from([repo]).map { urlString -> URL in
+      return URL(string: "https://api.github.com/repos/\(urlString)/events")!
+    }.map { url -> URLRequest in
+      return URLRequest(url: url)
+    }.flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
+      return URLSession.shared.rx.response(request: request)
+    }.share(replay: 1)
+    
+    response.filter { response, _ in
+      return 200..<300 ~= response.statusCode
+    }
+    .compactMap { _, data -> [Event]? in
+      return try? JSONDecoder().decode([Event].self, from: data)
+    }
+    .subscribe(onNext: { [weak self] newEvents in
+      self?.processEvents(newEvents)
+    })
+    .disposed(by: bag)
   }
   
   func processEvents(_ newEvents: [Event]) {
